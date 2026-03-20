084a435af657a153ce45d900a7d55ec9d32bff15
Napat-Tirmongkol <123386060+Napat-Tirmongkol@users.noreply.github.com>
2026-03-20 18:26:55 +0700

    Add files via upload

--- includes/header.php
+++ includes/header.php
@@ -19,6 +19,7 @@ function render_header(string $title = 'E-Vax'): void {
       <link rel="icon" href="data:,">
 
       <script src="https://cdn.tailwindcss.com"></script>
+	  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
       <script>
         // ปิด Warning ของ Tailwind
         if (window.console && window.console.log) {
